ddc7841c02baab31444b6dc72e8fdba7ab4699c9
tripleslicer <williamrevans@gmail.com>
2021-05-03 19:16:33 -0700

    practice creating react app

--- src/index.js
+++ src/index.js
@@ -6,3 +6,22 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const currentTime = new Date();
+
+const name = "Will";
+const lastName = "Evans";
+const thisYear = currentTime.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>
+      Created by {name} {lastName}
+    </p>
+    <p>Copyright {thisYear}</p>
+  </div>,
+  document.getElementById("root")
+);
